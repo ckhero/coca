786e8ddd867a44b9ad8e05a4c2a7a9aaf75ec2a5
ckhero <18801613198@163.com>
2018-02-08 22:00:38 +0800

    1

--- backend/modules/v1/swagger/models/uploadResult.php
+++ backend/modules/v1/swagger/models/uploadResult.php
@@ -1,34 +1,31 @@
 <?php
 
 /**
- * @SWG\Definition( type="object", @SWG\Xml(name="uploadResult"))
+ * @SWG\Definition(type="object", @SWG\Xml(name="uploadResult"))
  */
 class uploadResult
 {
-
-    
-
     /**
      * @SWG\Property(description="文件", example="jpg")
-     * @var file
+     * @var string
      */
     private $type;
 
     /**
      * @SWG\Property(description="文件名字", example="46b0ba04-ca6c-12e4-b90d-803aaeef5e39.mp4")
-     * @var file
+     * @var string
      */
     private $name;
 
     /**
      * @SWG\Property(description="文件原始名字", example="222.mp4")
-     * @var file
+     * @var string
      */
     private $original_name;
 
     /**
      * @SWG\Property(description="文件访问路径，数据库中保存此地址", example="uploads/20180202/46b0ba04-ca6c-12e4-b90d-803aaeef5e39.mp4")
-     * @var file
+     * @var string
      */
     private $path;
 
